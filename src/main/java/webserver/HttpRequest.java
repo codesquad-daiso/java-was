ed518deafffdbc2cd8fb931d272cc70d617dbdcf
David Chung <david215@berkeley.edu>
2020-03-17 15:00:38 +0900

    Implement HttpRequest class
    - Call the following methods in the constructor:
        - readRequestLine()
            - Read and assign data into appropriate fields.
        - readHeaders()
            - Utilize static method HttpRequestUtils.parseHeader().
        - readBody()
            - If an HTTP request has a body (not a GET method for now), read and
              assign body.

--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -1,8 +1,12 @@
 package webserver;
 
+import util.HttpRequestUtils;
 import util.HttpRequestUtils.Pair;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HttpRequest {
@@ -13,7 +17,10 @@ public class HttpRequest {
     private String requestBody;
 
     public HttpRequest(BufferedReader in) {
-        // construct an HttpRequest object using the input stream of a connection socket.
+        requestHeaders = new ArrayList<>();
+        readRequestLine(in);
+        readHeaders(in);
+        readBody(in);
     }
 
     public String getMethod() {
@@ -34,5 +41,54 @@ public class HttpRequest {
 
     public String getRequestBody() {
         return requestBody;
+    }
+
+    private void readRequestLine(BufferedReader in) {
+        try {
+            String[] requestLine = in.readLine().split(" ");
+            this.method = requestLine[0];
+            String url = requestLine[1];
+            if (url.equals("/")) {
+                url = "index.html";
+            }
+            this.url = url;
+            this.protocol = requestLine[2];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readHeaders(BufferedReader in) {
+        try {
+            String line = "";
+            while (!(line = in.readLine()).equals("")) { // \r\n is removed
+                requestHeaders.add(HttpRequestUtils.parseHeader(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readBody(BufferedReader in) {
+        if (!method.equals("GET")) { // or other HTTP methods that don't have a body
+            int contentLength = getContentLength();
+            char[] requestBody = new char[contentLength];
+            try {
+                int numCharsRead = in.read(requestBody, 0, contentLength);
+                // TODO: exception handling - numCharsRead must be equal to contentLength
+                this.requestBody = Arrays.toString(requestBody);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int getContentLength() {
+        for (Pair requestHeader : requestHeaders) {
+            if (requestHeader.getKey().equals("Content-Length")) {
+                return Integer.parseInt(requestHeader.getValue());
+            }
+        }
+        return -1; // TODO: exception handling
     }
 }
